cc39002225312a33932cf45868f27de095a3a6da
prsd503 <brink2wink@gmail.com>
2026-07-02 14:15:56 +0530

    Refactor service worker cache handling

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
 const CACHE_NAME = 'lifecraft-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
+  'index.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.3.1/fabric.min.js'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
